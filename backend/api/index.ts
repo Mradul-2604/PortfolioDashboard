ccdbd6ddb99e58478ef36a7bea2168c0e41a1f6e
Mradul <mradulmahajan@gmail.com>
2026-09-11 00:46:48 +0530

    Simplify Vercel backend deployment

--- backend/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from "../src/app";
-
-const app = createApp();
-
-import { Request, Response } from "express";
-
-export default function handler(req: Request, res: Response) {
-  return app(req, res);
-}
